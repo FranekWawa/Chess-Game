27ab96f804bc9274c939ca75a35369bc2b230aac
FranekWawa <68611842+FranekWawa@users.noreply.github.com>
2021-04-09 19:46:37 +0200

    pieces

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 let board = document.getElementById("board")
+var elem = document.createElement("img");
+elem.className = "image"
+elem.src = 'images/queen.png'
+var elem2 = document.createElement("img");
+elem2.className = "image"
+elem2.src = 'images/queen.png'
+
+let clickx = null
+let clicky = null
+let piece = null
 for(i = 0; i<8;i++){
     let row = document.createElement("div")
+    row.className = "row"
     for (j = 0;j<8;j++){
         let square = document.createElement("div")
         let x = j.toString()
@@ -11,10 +22,36 @@ for(i = 0; i<8;i++){
         if(i%2!=j%2){
             square.className += " black"
         }
-        square.addEventListener("click",function(){console.log(square.id)})
+        square.addEventListener("click",function(){
+            
+
+            if(square.hasChildNodes()){
+                clickx = j
+                clicky = i
+                piece = square.firstChild
+            }
+            else if(piece){
+                square.appendChild(piece)
+                piece = null
+            }
+            console.log(square.id)
+            console.log(piece)
+            console.log(square)
+            console.log(square.hasChildNodes())
+
+        })
+        if(i == 4 && j == 3){
+            square.appendChild(elem)
+        }
+        if(i == 3 && j == 4){
+            square.appendChild(elem2)
+        }
         row.appendChild(square)
+
+        
     }
     board.appendChild(row)
 }
+
 
 
