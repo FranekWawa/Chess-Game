5c208b41afc55b245ac42f4c0ad440035ae6f44c
FranekWawa <68611842+FranekWawa@users.noreply.github.com>
2021-05-16 19:55:35 +0200

    some changes

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-let board = document.getElementById("board")
-const initState = [
-["rb","nb","bb","qb","kb","bb","nb","rb"],
-["pb","pb","pb","pb","pb","pb","pb","pb"],
-[0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0],
-["pw","pw","pw","pw","pw","pw","pw","pw"],
-["rw","nw","bw","qw","kw","bw","nw","rw"]]
-
+let board = document.getElementById('board')
+import {initState} from './initState.js'
 let currState = initState
 let clickx = null
 let clicky = null
@@ -16,36 +7,32 @@ let piece = null
 let pieceType = null
 let whiteTurn = true
 
-for(i = 0; i<8;i++){
+for (let i = 0; i < 8; i++) {
+    let row = document.createElement('div')
+    row.className = 'row'
 
-    let row = document.createElement("div")
-    row.className = "row"
-
-    for (j = 0;j<8;j++){
-
-        let square = document.createElement("div")
+    for (let j = 0; j < 8;j++) {
+        let square = document.createElement('div')
         let x = j
         let y = i
-        square.id = x + ","+y
-        square.className = "box"
+        square.id = x + ',' + y
+        square.className = 'box'
 
-        if(i%2!=j%2){
-            square.className += " black"
+        if (i % 2 != j % 2) {
+            square.className += ' black'
         }
+        square.addEventListener('click', () => {
 
-        square.addEventListener("click",function(){
-
-            if(square.hasChildNodes()){
+            if (square.hasChildNodes()) {
 
                 clickx = x
                 clicky = y
                 piece = square.firstChild
 
-                if((whiteTurn && piece.src[piece.src.length - 5] !== 'w')||(!whiteTurn && piece.src[piece.src.length - 5] === 'w')){
+                if (whiteTurn !== (piece.src[piece.src.length - 5] === 'w')) {
                     piece = null
                 }
-            }
-            else if(piece){
+            } else if (piece) {
 
                 square.appendChild(piece)
                 pieceType = piece.src[piece.src.length - 6] + piece.src[piece.src.length - 5]
@@ -56,11 +43,11 @@ for(i = 0; i<8;i++){
             }
         })
 
-        if(initState[i][j] != 0){
+        if (initState[i][j] != 0) {
 
-            var elem = document.createElement("img");
-            elem.className = "image"
-            elem.src = 'images/'+initState[i][j]+'.png'
+            var elem = document.createElement('img')
+            elem.className = 'image'
+            elem.src = 'images/' + initState[i][j] + '.png'
             square.appendChild(elem)
         }
 
@@ -69,6 +56,3 @@ for(i = 0; i<8;i++){
 
     board.appendChild(row)
 }
-
-
-
